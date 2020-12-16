7326bb8d337fc9f39a31f702afa3768267402364
Apurv Jyotirmay <apjyotirmay@gmail.com>
2020-12-16 21:35:14 +0530

    php cs fix

--- index.php
+++ index.php
@@ -9,23 +9,24 @@ $xml = new SimpleXMLElement('<urlset xmlns="http://www.sitemaps.org/schemas/site
 $or['url']=array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1');
 to_xml($xml, $or);
 foreach ($types['webapp']['searchable_types'] as $tp) {
-	$posts=$sql->executeSQL("SELECT `id`, `content`->>'$.slug' `slug`, `updated_on` FROM `data` WHERE `content`->'$.content_privacy'='public' && `content`->'$.type'='$tp' ORDER BY `id` DESC");
-	foreach ($posts as $post) {
-		$or['url']=array('loc'=>BASE_URL.'/'.$tp.'/'.$post['slug'], 'lastmod'=>date('Y-m-d', $post['updated_on']), 'priority'=>'0.7');
-		to_xml($xml, $or);
-	}
+    $posts=$sql->executeSQL("SELECT `id`, `content`->>'$.slug' `slug`, `updated_on` FROM `data` WHERE `content`->'$.content_privacy'='public' && `content`->'$.type'='$tp' ORDER BY `id` DESC");
+    foreach ($posts as $post) {
+        $or['url']=array('loc'=>BASE_URL.'/'.$tp.'/'.$post['slug'], 'lastmod'=>date('Y-m-d', $post['updated_on']), 'priority'=>'0.7');
+        to_xml($xml, $or);
+    }
 }
 print $xml->asXML();
-function to_xml(SimpleXMLElement $object, array $data) {
+function to_xml(SimpleXMLElement $object, array $data)
+{
     foreach ($data as $key => $value) {
         if (is_array($value)) {
             $new_object = $object->addChild($key);
             to_xml($new_object, $value);
         } else {
-            if (is_int($key))
+            if (is_int($key)) {
                 $key = "key_$key";
+            }
             $object->addChild($key, $value);
         }
     }
 }
-?>
